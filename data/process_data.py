11ea1947c24fff066d3dbba6e439dbe1e7a4eb3b
Mantraraj <mantraraj.dash@zs.com>
2019-08-27 20:29:01 +0530

    adding processing and modeling codes

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,44 @@
 import sys
-
+import pandas as pd
+import sqlalchemy
+import numpy as np
 
 def load_data(messages_filepath, categories_filepath):
-    pass
-
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = messages.drop_duplicates().merge(categories.drop_duplicates(),how='outer')
+    return df
 
 def clean_data(df):
-    pass
+    categories = df['categories'].str.split(pat=';',expand=True)
+    row = categories.loc[0,:].copy()
+    category_colnames = row.apply(lambda x: x[:-2])
+    for column in category_colnames:
+    # set each value to be the last character of the string
+        categories[column] = categories[column].astype(str).str[-1]
+        
+        # set values to 0 or 1 (anything above 1 is 1)
+        categories[column] = categories[column].apply(lambda x: 1 if int(x)!=0 else 0)
+        
+        # convert column from string to numeric
+        categories[column] = categories[column].astype(np.int8)
+    
+    # drop the original categories column from `df`
+    df.drop(columns='categories',inplace=True)
+
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df,categories],axis=1)
+
+    # drop duplicates
+    df.drop_duplicates(inplace=True)
+
+
+
 
 
 def save_data(df, database_filename):
-    pass  
+    engine = sqlalchemy.create_engine(database_filename)
+    df.to_sql('CleanMessages', engine, index=False, if_exists='replace')  
 
 
 def main():
--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,91 @@
+# import libraries
 import sys
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine,MetaData
 
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+
+import re
+
+import sklearn
+from sklearn.pipeline import Pipeline,FeatureUnion
+import xgboost
+from sklearn.multioutput import MultiOutputClassifier
+
+from sklearn.model_selection import train_test_split,GridSearchCV
+from sklearn.metrics import classification_report,precision_score,recall_score,roc_auc_score,balanced_accuracy_score
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.decomposition import LatentDirichletAllocation,TruncatedSVD
 
 def load_data(database_filepath):
-    pass
+    
+    # load data from database
+    engine = create_engine(database_filepath)
+    df = pd.read_sql_table('CleanMessages',con=engine)
+    X = df['message'].values
+    Y = df[[x for x in df.columns.values if x not in ['id','genre','message','original']]].values
+    category_names = df[[x for x in df.columns.values if x not in ['id','genre','message','original']]].columns.values
+    Y.drop(columns='child_alone',inplace=True)
+
+    return X,Y,category_names
 
 
 def tokenize(text):
-    pass
+    
+    stop_words = stopwords.words("english")
+    lemmatizer = WordNetLemmatizer()
+    
+    # normalize case and remove punctuation
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
+    
+    # tokenize text
+    tokens = word_tokenize(text)
+    
+    # lemmatize andremove stop words
+    tokens = [lemmatizer.lemmatize(word) for word in tokens if word not in stop_words]
+
+    return tokens
 
 
 def build_model():
-    pass
+    
+    pipeline = Pipeline([
+    ('countvec',CountVectorizer(tokenizer=tokenize)),
+    ('tfidf',TfidfTransformer()),
+    #     ('lsa',TruncatedSVD(random_state=42,n_components=100)),
+    ('clf',MultiOutputClassifier(xgboost.XGBClassifier(random_state=42,n_jobs=-1),n_jobs=-1))
+    ])
+
+    return pipeline
+
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    
+    preds_test = model.predict(X_test)
+
+    for i,col in enumerate(category_names):
+        print(f'Predictions for {col}')
+        print(classification_report(y_test[col],preds_test[...,i]))
+
+    print(f'Precision score: {precision_score(Y_test,preds_test,average="macro")}')
+    print(f'Recall: {recall_score(Y_test,preds_test,average="macro")}')
+    print(f'ROC: {roc_auc_score(Y_test,preds_test,average="macro")}')
 
 
 def save_model(model, model_filepath):
-    pass
+    
+    #TODO add condition to check if file already exists
+    if ~file_exists:
+        joblib.dump(model, model_filepath, compress = 1)
 
 
 def main():
